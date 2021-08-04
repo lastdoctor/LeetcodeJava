4791c169aed02a62ca43db5a75f9b519c1e62f2e
github.com/lastdoctor <dom.ru0@yandex.ru>
2021-08-04 06:03:08 +0500

    119

--- src/com/company/dynamic/PascalsTriangleII_119/Solution.java
+++ src/com/company/dynamic/PascalsTriangleII_119/Solution.java
@@ -5,10 +5,28 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-public class Solution {
+class Solution {
     public List<Integer> getRow(int rowIndex) {
+        if(rowIndex == 0) return Arrays.asList(1);
+
         List<List<Integer>> triangle = new ArrayList<>();
-        if (rowIndex == 0) return Arrays.asList(1);
-        return triangle.get(0);
+
+        List<Integer> firstRow = new ArrayList<>();
+        firstRow.add(1);
+        triangle.add(firstRow);
+
+        for (int i = 1; i < rowIndex + 1; i++) {
+            List<Integer> prevRow = triangle.get(i-1);
+            List<Integer> row = new ArrayList<>();
+
+            row.add(1);
+            for(int j = 1; j < i; j++) {
+                row.add(prevRow.get(j-1) + prevRow.get(j));
+            }
+            row.add(1);
+            triangle.add(row);
+        }
+
+        return triangle.get(rowIndex);
     }
 }
