fd4e61f8227f59035fca69cf6029032350af2315
github.com/lastdoctor <dom.ru0@yandex.ru>
2021-08-05 21:21:36 +0500

    344

---
+++ src/com/company/string/ReverseString_344/Solution.java
@@ -0,0 +1,13 @@
+package com.company.string.ReverseString_344;
+
+class Solution {
+    public void reverseString(char[] s) {
+        int left = 0;
+        int right = s.length - 1;
+        while (left < right) {
+            char tmp = s[left];
+            s[left] = s[right];
+            s[right] = s[tmp];
+        }
+    }
+}
