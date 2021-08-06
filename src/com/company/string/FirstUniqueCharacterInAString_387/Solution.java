43d48886f1daedb9b37dc02bdf3f3368717c1958
github.com/lastdoctor <dom.ru0@yandex.ru>
2021-08-06 06:19:58 +0500

    387

---
+++ src/com/company/string/FirstUniqueCharacterInAString_387/Solution.java
@@ -0,0 +1,22 @@
+package com.company.string.FirstUniqueCharacterInAString_387;
+
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int firstUniqChar(String s) {
+        Map<Character, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            map.put(c, map.getOrDefault(c, 0) + 1);
+        }
+
+        for (int i = 0; i < s.length(); i++) {
+            if(map.get(s.charAt(i)) == 1){
+                return i;
+            }
+        }
+        return -1;
+    }
+}
