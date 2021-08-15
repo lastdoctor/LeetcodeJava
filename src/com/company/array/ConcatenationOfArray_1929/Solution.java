c42389e088adfd7c0a73a91d406d0ae3581437c2
github.com/lastdoctor <dom.ru0@yandex.ru>
2021-08-15 19:23:30 +0500

    1929

---
+++ src/com/company/array/ConcatenationOfArray_1929/Solution.java
@@ -0,0 +1,14 @@
+package com.company.array.ConcatenationOfArray_1929;
+
+class Solution {
+    public int[] getConcatenation(int[] nums) {
+        int[] ans = new int[nums.length * 2];
+        int secondPart = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            ans[i] = nums[i];
+            ans[secondPart] = nums[i];
+            secondPart++;
+        }
+        return ans;
+    }
+}
