0445246ebf615a729411f4fef98d0c56a07d67b5
github.com/lastdoctor <dom.ru0@yandex.ru>
2021-08-15 19:02:34 +0500

    434

---
+++ src/com/company/string/NumberOfSegmentsInAString_434/Solution.java
@@ -0,0 +1,15 @@
+package com.company.string.NumberOfSegmentsInAString_434;
+
+import java.util.Arrays;
+
+public class Solution {
+    public int countSegments(String s) {
+        s = s.trim();
+        if (s.equals("")){
+            return 0;
+        }
+        // \\s+ - matches sequence of one or more whitespace characters.
+        System.out.println(Arrays.toString(s.split("\\s+")));
+        return s.split("\\s+").length;
+    }
+}
