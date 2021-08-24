0c19cf2fe507bfc15781995061a70e92d91ad625
github.com/lastdoctor <dom.ru0@yandex.ru>
2021-08-24 16:20:00 +0500

    771

---
+++ src/com/company/string/JewelsAndStones_771/Solution.java
@@ -0,0 +1,22 @@
+package com.company.string.JewelsAndStones_771;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int numJewelsInStones(String jewels, String stones) {
+        Set<Character> set = new HashSet<>();
+        for (char c : jewels.toCharArray()) {
+            set.add(c);
+        }
+
+        int ans = 0;
+        for (char c : stones.toCharArray()) {
+            if(set.contains(c)){
+                ans++;
+            }
+        }
+        return ans;
+    }
+}
