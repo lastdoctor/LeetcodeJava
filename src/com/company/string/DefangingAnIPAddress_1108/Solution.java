662c3789f1e1daadc9360a598c86018d9e9f6a30
github.com/lastdoctor <dom.ru0@yandex.ru>
2021-08-24 16:01:02 +0500

    1108

---
+++ src/com/company/string/DefangingAnIPAddress_1108/Solution.java
@@ -0,0 +1,16 @@
+package com.company.string.DefangingAnIPAddress_1108;
+
+// Time O(n) Space O(1);
+class Solution {
+    public String defangIPaddr(String address) {
+        String ans = "";
+        for (char c : address.toCharArray()) {
+            if (c != '.') {
+                ans += c;
+            } else {
+                ans += "[.]";
+            }
+        }
+        return ans;
+    }
+}
