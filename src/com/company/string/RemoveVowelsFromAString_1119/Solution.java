7de9edc3787c937c94a70bece2d5371489c5b829
github.com/lastdoctor <dom.ru0@yandex.ru>
2021-08-24 15:51:56 +0500

    1119

---
+++ src/com/company/string/RemoveVowelsFromAString_1119/Solution.java
@@ -0,0 +1,24 @@
+package com.company.string.RemoveVowelsFromAString_1119;
+
+import java.util.HashSet;
+import java.util.Set;
+
+// Time O(n) and Space O(1);
+class Solution {
+    public String removeVowels(String s) {
+        Set<Character> vowels = new HashSet<>();
+        vowels.add('a');
+        vowels.add('e');
+        vowels.add('i');
+        vowels.add('o');
+        vowels.add('u');
+
+        String ans = "";
+        for (char c : s.toCharArray()){
+            if(!vowels.contains(c)){
+                ans += c;
+            }
+        }
+        return ans;
+    }
+}
