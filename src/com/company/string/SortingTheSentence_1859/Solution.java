55dc50dc4967be8d68ea66a285347d371395c76b
github.com/lastdoctor <dom.ru0@yandex.ru>
2021-08-24 19:04:35 +0500

    1859

---
+++ src/com/company/string/SortingTheSentence_1859/Solution.java
@@ -0,0 +1,28 @@
+package com.company.string.SortingTheSentence_1859;
+
+import java.util.Arrays;
+
+class Solution {
+    public String sortSentence(String s) {
+        String[] arrtemp = s.split(" ");
+        String[] ans = new String[arrtemp.length];
+
+        String temp = "";
+        for (int i = 0; i < s.length(); i++) {
+            if(IsNum(s.charAt(i))) {
+                ans[Integer.parseInt(String.valueOf(s.charAt(i))) - 1] = temp;
+                temp = "";
+                i++;
+            } else {
+                temp += s.charAt(i);
+            }
+        }
+
+        return String.join(" ", ans);
+    }
+
+    private boolean IsNum(char c) {
+        return c - '0' >= 0 && c - '9' <= 0;
+    }
+}
+
