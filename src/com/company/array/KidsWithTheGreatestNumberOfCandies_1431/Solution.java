a4e97ce805b9ef581a31acf3719517b72e746324
Susa Kubatov <Susa Kubatov>
2021-09-02 22:37:57 +0500

    1431

--- src/com/company/array/KidsWithTheGreatestNumberOfCandies_1431/Solution.java
+++ src/com/company/array/KidsWithTheGreatestNumberOfCandies_1431/Solution.java
@@ -1,9 +1,24 @@
-//package com.company.array.KidsWithTheGreatestNumberOfCandies_1431;
-//
-//import java.util.List;
-//
-//class Solution {
-//    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
-//        return
-//    }
-//}
+package com.company.array.KidsWithTheGreatestNumberOfCandies_1431;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<Boolean> kidsWithCandies(int[] candies, int extraCandies) {
+        List<Boolean> ans = new ArrayList<>();
+        int max = 0;
+        for (int num : candies) {
+            if (num > max) {
+                max = num;
+            }
+        }
+        for (int num : candies) {
+            if (num + extraCandies >= max) {
+                ans.add(true);
+            } else {
+                ans.add(false);
+            }
+        }
+        return ans;
+    }
+}
---
+++ src/com/company/competive_training/bubble_sort/Solution.java
@@ -0,0 +1,45 @@
+package com.company.competive_training.bubble_sort;
+
+import java.util.Scanner;
+
+// Time Complexity On2 and Space Complexity O(1);
+public class Solution {
+    public static void bubble_sort(int[] arr) {
+//        int[] arr = new int[1000];
+//        Scanner scan = new Scanner(System.in);
+//        int n = scan.nextInt();
+//        for (int i = 0; i < n; i++) {
+//            arr[i] = scan.nextInt();
+//        }
+
+        for (int i = arr.length - 1; i >= 1; i--) {
+            for (int j = 0; j < i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+
+    public static void main(String... args) {
+        Solution s = new Solution();
+        int[] arr = {5, 1, 4, 2, 8,};
+        s.bubble_sort(arr);
+    }
+}
+
+
+// 5 1 4 2 8
+// 1 5 4 2 8
+// 1 4 5 2 8
+// 1 4 2 5 8
+// 1 4 2 5 8
+// 1 2 4 5 8
+// 1 2 4 5 8
+// 1 2 4 5 8
