ef65acefd1179b30473e0b87fe3eb7a22471189b
Susa Kubatov <Susa Kubatov>
2021-09-08 21:31:11 +0500

    1773

---
+++ src/com/company/array/ShortestWordDistance_243/Solution.java
@@ -0,0 +1,23 @@
+package com.company.array.ShortestWordDistance_243;
+
+class Solution {
+    public int shortestDistance(String[] wordsDict, String word1, String word2) {
+        Map<String, Integer> map = new HashMap<>();
+
+        int idx = 0;
+        for (String c : wordsDict) {
+            if (!map.containsKey(c)) {
+                map.put(c, idx);
+            } else {
+                map.put(c, map.get(c) + idx);
+            }
+            idx++;
+        }
+
+
+
+        System.out.println(map);
+
+        return 1;
+    }
+}
