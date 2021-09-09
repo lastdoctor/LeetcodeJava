fc00f25785551f7e9bd83b6abdefc71fda8f917c
Susa Kubatov <Susa Kubatov>
2021-09-10 01:10:08 +0500

    242

--- src/com/company/string/ValidAnagram_242/Solution.java
+++ src/com/company/string/ValidAnagram_242/Solution.java
@@ -1,5 +1,8 @@
 package com.company.string.ValidAnagram_242;
 
+import java.util.HashMap;
+import java.util.Map;
+
 class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) {
@@ -17,6 +20,27 @@ class Solution {
                 return false;
             }
         }
+        return true;
+    }
+}
+
+class Solution1 {
+    public boolean isAnagram(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+        Map<Character, Integer> s1 = new HashMap<>();
+        Map<Character, Integer> s2 = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            s1.put(s.charAt(i), s1.getOrDefault(s.charAt(i), 0) + 1);
+            s2.put(t.charAt(i), s2.getOrDefault(t.charAt(i), 0) + 1);
+        }
+
+        for (char c : s1.keySet()) {
+            if (!s2.containsKey(c) || !s1.get(c).equals(s2.get(c))) return false;
+        }
+
         return true;
     }
 }
