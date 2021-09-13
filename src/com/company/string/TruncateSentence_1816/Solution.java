7415486b3241c160f2aaaaa183bfe39df7d92c8e
Susa Kubatov <Susa Kubatov>
2021-09-13 22:33:55 +0500

    1816

---
+++ src/com/company/string/TruncateSentence_1816/Solution.java
@@ -0,0 +1,15 @@
+package com.company.string.TruncateSentence_1816;
+
+// Time O(n)
+// Space O(n)
+class Solution {
+    public String truncateSentence(String s, int k) {
+        StringBuilder ans = new StringBuilder();
+        for (char c : s.toCharArray()) {
+            if (k == 0) break;
+            if (c == ' ') k--;
+            ans.append(c);
+        }
+        return ans.toString().trim();
+    }
+}
