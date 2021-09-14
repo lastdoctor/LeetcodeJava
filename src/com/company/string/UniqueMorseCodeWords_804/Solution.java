3de691d59dbe2fb415a9637ad9741d14bb2c542b
Susa Kubatov <Susa Kubatov>
2021-09-15 01:46:10 +0500

    804

---
+++ src/com/company/string/UniqueMorseCodeWords_804/Solution.java
@@ -0,0 +1,23 @@
+package com.company.string.UniqueMorseCodeWords_804;
+
+import java.util.HashSet;
+import java.util.Set;
+
+class Solution {
+    public int uniqueMorseRepresentations(String[] words) {
+        String[] morse = {".-","-...","-.-.","-..",".","..-.","--.","....","..",".---","-.-",".-..","--","-.","---",".--.","--.-",".-.","...","-","..-","...-",".--","-..-","-.--","--.."};
+
+        Set<String> set = new HashSet<>();
+
+        for (String str : words) {
+            StringBuilder sb = new StringBuilder();
+            for (char c : str.toCharArray()) {
+                int i = c - 'a';
+                sb.append(morse[i]);
+            }
+            set.add(sb.toString());
+        }
+
+        return set.size();
+    }
+}
