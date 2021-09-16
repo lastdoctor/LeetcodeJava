8a67ee191f64cf053d9c68731caca4134ade005b
Susa Kubatov <Susa Kubatov>
2021-09-16 20:19:34 +0500

    1047

---
+++ src/com/company/Solutions/Stack/RemoveAllAdjacentDuplicatesInString_1047/Solution.java
@@ -0,0 +1,24 @@
+package com.company.Solutions.Stack.RemoveAllAdjacentDuplicatesInString_1047;
+
+import java.util.Stack;
+
+class Solution {
+    public String removeDuplicates(String s) {
+        Stack<Character> st = new Stack();
+        StringBuilder ans = new StringBuilder();
+
+        char prev = s.charAt(0);
+        for (char c : s.toCharArray()) {
+            if (!st.isEmpty() && st.peek() == c) {
+                st.pop();
+            } else {
+                st.push(c);
+            }
+        }
+
+        for (char c : st) {
+            ans.append(c);
+        }
+        return ans.toString();
+    }
+}
