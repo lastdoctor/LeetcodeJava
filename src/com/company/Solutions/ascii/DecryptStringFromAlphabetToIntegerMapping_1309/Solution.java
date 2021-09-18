342b5df63c446319ae0013f788dadec6d13d0d06
Susa Kubatov <Susa Kubatov>
2021-09-19 00:51:04 +0500

    1309

---
+++ src/com/company/Solutions/ascii/DecryptStringFromAlphabetToIntegerMapping_1309/Solution.java
@@ -0,0 +1,21 @@
+package com.company.Solutions.ascii.DecryptStringFromAlphabetToIntegerMapping_1309;
+
+class Solution {
+    public String freqAlphabets(String s) {
+        StringBuilder ans = new StringBuilder();
+
+        for (int i = 0; i < s.length(); i++) {
+            int firstDigit = Character.getNumericValue(s.charAt(i));
+
+            if(i + 2 < s.length() && s.charAt(i + 2) == '#') {
+                int secondDigit = Character.getNumericValue(s.charAt(i + 1));
+                ans.append((char)('j' + firstDigit * 10 + secondDigit - 10));
+                i += 2;
+            } else {
+                ans.append((char)('a' +firstDigit - 1));
+            }
+        }
+
+        return ans.toString();
+    }
+}
