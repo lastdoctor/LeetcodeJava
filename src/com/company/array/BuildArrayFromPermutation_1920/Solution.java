ecd5cf02731530c704a8c40dddd7c2d1b3baa937
Susa Kubatov <Susa Kubatov>
2021-09-24 03:17:17 +0500

    1920

--- src/com/company/array/BuildArrayFromPermutation_1920/Solution.java
+++ src/com/company/array/BuildArrayFromPermutation_1920/Solution.java
@@ -1,5 +1,7 @@
 package com.company.array.BuildArrayFromPermutation_1920;
 
+
+// Space O(n) Time O(n)
 class Solution {
     public int[] buildArray(int[] nums) {
         int[] ans = new int[nums.length];
@@ -7,5 +9,20 @@ class Solution {
             ans[i] = nums[nums[i]];
         }
         return ans;
+    }
+}
+
+// Space O(1) Time O(2n)
+class Solution1 {
+    public int[] buildArray(int[] nums) {
+        int n = nums.length;
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] += (nums[nums[i]]%n)*n;
+        }
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] /= n;
+        }
+
+        return nums;
     }
 }
