f06821483022ab252bff4ee0ec52d469df29be30
Susa Kubatov <dom.ru0@yandex.ru>
2021-09-27 02:29:23 +0500

    1313

---
+++ src/com/company/array/DecompressRunLengthEncodedList_1313/Solution.java
@@ -0,0 +1,21 @@
+package com.company.array.DecompressRunLengthEncodedList_1313;
+
+import java.util.Arrays;
+
+class Solution {
+    public int[] decompressRLElist(int[] nums) {
+        int n = 0;
+
+        for (int i = 0; i < nums.length; i+=2) {
+            n += nums[i];
+        }
+
+        int[] ans = new int[n];
+        int idx = 0;
+        for (int i = 0; i < nums.length; i+= 2) {
+            Arrays.fill(ans, idx, idx + nums[i], nums[i + 1]);
+            idx += nums[i];
+        }
+        return ans;
+    }
+}
