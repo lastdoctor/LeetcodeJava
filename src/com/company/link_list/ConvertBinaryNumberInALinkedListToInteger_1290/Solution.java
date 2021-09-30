cbd142a6d4896a8931884a79f593ab6c7dc04ae2
Susa Kubatov <dom.ru0@yandex.ru>
2021-09-30 22:53:46 +0500

    1290

---
+++ src/com/company/link_list/ConvertBinaryNumberInALinkedListToInteger_1290/Solution.java
@@ -0,0 +1,48 @@
+package com.company.link_list.ConvertBinaryNumberInALinkedListToInteger_1290;
+
+
+// Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+// Space O(1) Space O(n)
+// Binary Representation
+class Solution {
+    public int getDecimalValue(ListNode head) {
+        int ans = head.val;
+        while(head.next != null) {
+            ans = ans * 2 + head.next.val;
+            head = head.next;
+        }
+
+        return ans;
+    }
+}
+// Space O(1) Space O(n)
+// Bit Manipulation
+class Solution1 {
+    public int getDecimalValue(ListNode head) {
+        int ans = head.val;
+
+        while(head.next != null) {
+            ans = ans << 1;
+            ans = ans | head.next.val;
+            head = head.next;
+        }
+
+        return ans;
+    }
+}
