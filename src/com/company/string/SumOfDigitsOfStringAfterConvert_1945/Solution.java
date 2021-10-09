99bf8ab3e0fbe63f86209037f132b99ac40498bf
Susa Kubatov <dom.ru0@yandex.ru>
2021-10-10 02:21:30 +0500

    1945

---
+++ src/com/company/string/SumOfDigitsOfStringAfterConvert_1945/Solution.java
@@ -0,0 +1,26 @@
+package com.company.string.SumOfDigitsOfStringAfterConvert_1945;
+
+class Solution {
+    public int getLucky(String s, int k) {
+        StringBuilder b = new StringBuilder();
+        int res = 0;
+
+        for (char c: s.toCharArray()) {
+            b.append(c - 'a' + 1);
+        }
+
+        for (int i = 0; i < k; i++) {
+            for (char l: b.toString().toCharArray()) {
+                res += l - '0';
+            }
+
+            if (k > 1 && i != k - 1) {
+                b.setLength(0);
+                b.append(res);
+                res = 0;
+            }
+        }
+
+        return res;
+    }
+}
