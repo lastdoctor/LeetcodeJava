2deb29bd5d81e68db25debfede8257c404e7c6b8
Susa Kubatov <dom.ru0@yandex.ru>
2021-10-09 19:54:00 +0500

    206

---
+++ src/com/company/link_list/ReverseLinkedList_206/Solution.java
@@ -0,0 +1,36 @@
+package com.company.link_list.ReverseLinkedList_206;
+
+
+//  Definition for singly-linked list.
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
+// Space O(1) Time O(n);
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode prev = null;
+
+        while (head != null) {
+            ListNode next_node = head.next;
+            head.next = prev;
+            prev = head;
+            head = next_node;
+        }
+
+        return prev;
+    }
+}
