dc1023310278bbeb4419e7302848a1e1fd885640
Susa Kubatov <dom.ru0@yandex.ru>
2021-10-11 02:18:02 +0500

    1518

---
+++ src/com/company/math/WaterBottles_1518/Solution.java
@@ -0,0 +1,13 @@
+package com.company.math.WaterBottles_1518;
+
+class Solution {
+    public int numWaterBottles(int numBottles, int numExchange) {
+        int ans = numBottles, numNew;
+        while (numBottles >= numExchange) {
+            numNew = numBottles / numExchange;
+            ans += numNew;
+            numBottles = numNew + (numBottles % numExchange);
+        }
+        return ans;
+    }
+}
