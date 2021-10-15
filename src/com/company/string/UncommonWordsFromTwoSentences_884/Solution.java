b026cd288977936a25870f98b80eba02b7e41bff
Susa Kubatov <dom.ru0@yandex.ru>
2021-10-16 04:35:52 +0500

    884

--- src/com/company/string/UncommonWordsFromTwoSentences_884/Solution.java
+++ src/com/company/string/UncommonWordsFromTwoSentences_884/Solution.java
@@ -18,3 +18,19 @@ class Solution {
         return ans.toArray(new String[ans.size()]);
     }
 }
+
+class Solution1 {
+    public String[] uncommonFromSentences(String A, String B) {
+        Map<String, Integer> count = new HashMap<>();
+        for (String word : (A + " " + B).split(" ")) {
+            count.put(word, count.getOrDefault(word, 0) + 1);
+        }
+        List <String> ans = new ArrayList<>();
+        for (String word : count.keySet()) {
+            if (count.get(word) == 1)
+                ans.add(word);
+        }
+
+        return ans.toArray(new String[0]);
+    }
+}
