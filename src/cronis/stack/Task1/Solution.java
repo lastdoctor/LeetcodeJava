726cd5501ef4b5b884843b10a65e6a209c1575ba
Susa Kubatov <dom.ru0@yandex.ru>
2021-10-23 00:42:09 +0500

    Solution

--- src/cronis/stack/Task1/Solution.java
+++ src/cronis/stack/Task1/Solution.java
@@ -1,14 +1,16 @@
 package cronis.stack.Task1;
 
+
+import java.util.Arrays;
+
 class Stack {
     private int STACK_TOP = -1;
-    private int STACK_SIZE;
-    int[] STACK = new int[STACK_SIZE];
-    int stackSize = 0;
+    private int stackSize = 0;
+    private int STACK_SIZE = 5;
+    private int[] STACK = new int[this.STACK_SIZE];
 
-    Stack(int stack_size) {
-        this.STACK_SIZE = stack_size;
-    }
+
+
 
     void push(int value) throws Exception {
         if (this.STACK_TOP == this.STACK_SIZE - 1)
@@ -40,8 +42,41 @@ class Stack {
     int size() {
         return this.stackSize;
     }
+
+    @Override
+    public String toString() {
+        return "Stack{" +
+                "STACK_TOP=" + STACK_TOP +
+                ", STACK_SIZE=" + STACK_SIZE +
+                ", STACK=" + Arrays.toString(STACK) +
+                ", stackSize=" + stackSize +
+                '}';
+    }
 }
 
 class Solution {
-//    pb
+    public static void main(String... args) throws Exception {
+        Stack st1 = new Stack();
+        Stack st2 = new Stack();
+        st1.push(1);
+        st1.push(5);
+        st1.push(0);
+        st1.push(-60);
+        st1.push(20);
+
+        System.out.println("Unsorted stack :" + st1);
+        System.out.println(st2);
+
+        while (st1.isEmpty()) {
+            int st1peak = st1.pop();
+
+            while (!st2.isEmpty() && st2.peek() > st1peak) {
+                st1.push(st1.pop());
+            }
+            st2.push(st1peak);
+        }
+
+        System.out.println("Sorted stack :" + st1);
+        System.out.println(st2);
+    }
 }
