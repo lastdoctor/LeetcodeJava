9001e35d730a4d9e47e95377342f864272e58769
Susa Kubatov <dom.ru0@yandex.ru>
2021-10-22 23:16:41 +0500

    Stack

--- src/cronis/stack/Task1/Solution.java
+++ src/cronis/stack/Task1/Solution.java
@@ -2,18 +2,46 @@ package cronis.stack.Task1;
 
 class Stack {
     private int STACK_TOP = -1;
-    private int STACK_SIZE = 3;
+    private int STACK_SIZE;
     int[] STACK = new int[STACK_SIZE];
     int stackSize = 0;
 
+    Stack(int stack_size) {
+        this.STACK_SIZE = stack_size;
+    }
+
     void push(int value) throws Exception {
-        if (STACK_TOP == STACK_SIZE - 1)
+        if (this.STACK_TOP == this.STACK_SIZE - 1)
             throw new Exception("Stack overflow");
         this.STACK_TOP += 1;
         STACK[this.STACK_TOP] = value;
         this.stackSize++;
     }
+
+    int pop() throws Exception {
+        if (this.STACK_TOP == -1)
+            throw new Exception("Stack is empty");
+        int value = this.STACK[this.STACK_TOP];
+        this.STACK_TOP--;
+        this.stackSize--;
+        return value;
+    }
+
+    int peek() throws Exception {
+        if (this.STACK_TOP == -1)
+            throw new Exception("Stack is empty");
+        return this.STACK[this.STACK_TOP];
+    }
+
+    boolean isEmpty() {
+        return this.STACK_TOP == -1;
+    }
+
+    int size() {
+        return this.stackSize;
+    }
 }
 
 class Solution {
+//    pb
 }
