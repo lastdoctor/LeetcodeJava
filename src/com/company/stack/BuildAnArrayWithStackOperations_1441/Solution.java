7edd9d0d54038cf66bb48246dd5cd5454f3315e0
Susa Kubatov <dom.ru0@yandex.ru>
2021-10-30 04:47:25 +0500

    1441

---
+++ src/com/company/stack/BuildAnArrayWithStackOperations_1441/Solution.java
@@ -0,0 +1,20 @@
+package com.company.stack.BuildAnArrayWithStackOperations_1441;
+
+import java.util.*;
+
+class Solution {
+    public List<String> buildArray(int[] target, int n) {
+        List<String> ans = new ArrayList<>();
+
+        for (int i = 1, idx = 0; idx < target.length; i++) {
+            ans.add("Push");
+            if (target[idx] != i) {
+                ans.add("Pop");
+            } else {
+                idx++;
+            }
+        }
+
+        return ans;
+    }
+}
