e867c57253b7fb33f3c2ce63476020e0f6d252ef
Susa Kubatov <dom.ru0@yandex.ru>
2021-10-31 04:31:41 +0500

    task 2

--- src/cronis/recursion/MaxArr.java
+++ src/cronis/recursion/MaxArr.java
@@ -1,12 +1,16 @@
 package cronis.recursion;
 
-//public class MaxArr {
-//    public static void main(String[] args) {
-//        int max = new SumToN().sumToN(1234);
-//        System.out.println();
-//    }
-//
-//    private int max(int[] arr) {
-//
-//    }
-//}
+public class MaxArr {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 4, 22, -1};
+        int max = new MaxArr().maxInt(arr, arr.length);
+        System.out.println(max);
+    }
+
+    private int maxInt(int[] arr, int end) {
+        if (end == 0) return 0;
+        if (end == 1) return arr[0];
+        if (end == 2) return Math.max(arr[0], arr[1]);
+        return Math.max(arr[end - 1], maxInt(arr, end -1));
+    }
+}
