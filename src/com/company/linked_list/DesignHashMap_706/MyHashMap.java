a65661b26db3f0be3df822d2f6e8143986b649cd
Susa Kubatov <dom.ru0@yandex.ru>
2021-11-21 13:32:27 +0500

    705

---
+++ src/com/company/linked_list/DesignHashMap_706/MyHashMap.java
@@ -0,0 +1,33 @@
+package com.company.linked_list.DesignHashMap_706;
+
+import java.util.Arrays;
+
+class MyHashMap {
+    int[] bucket;
+    public MyHashMap() {
+        int bucketSize = (int)Math.pow(10, 6) + 1;
+        bucket = new int[bucketSize];
+        Arrays.fill(bucket, -1);
+    }
+
+
+    public void put(int key, int value) {
+        bucket[key] = value;
+    }
+
+    public int get(int key) {
+        return bucket[key];
+    }
+
+    public void remove(int key) {
+        bucket[key] = -1;
+    }
+}
+
+/**
+ * Your MyHashMap object will be instantiated and called as such:
+ * MyHashMap obj = new MyHashMap();
+ * obj.put(key,value);
+ * int param_2 = obj.get(key);
+ * obj.remove(key);
+ */
--- src/cronis/hash_table/HashTable.java
+++ src/cronis/hash_table/HashTable.java
@@ -1,6 +1,9 @@
 package cronis.hash_table;
 
+import java.util.Arrays;
+
 // Обьект связанного списка из пар ключ значение
+// Метод цепочек
 class Bucket {
     public String key;
     public int value;
@@ -10,15 +13,24 @@ class Bucket {
         this.key = key;
         this.value = value;
     }
+
+    @Override
+    public String toString() {
+        return "Bucket{" +
+                "key='" + key + '\'' +
+                ", value=" + value +
+                ", next=" + next +
+                '}';
+    }
 }
 
 class HashTable {
-    int[] primes = {5, 11, 23, 47, 97};
-    int size = 0;
-    int capacityIndex = 0;
-    Bucket[] buckets = new Bucket[primes[capacityIndex]];
+    private int[] primes = {5, 11, 23, 47, 97};
+    private int size = 0;
+    private int capacityIndex = 0;
+    private Bucket[] buckets = new Bucket[primes[capacityIndex]];
 
-    int hashCode(String str) {
+    public int hashCode(String str) {
         int hash = 0;
         for (int i = 0; i < str.length(); i++) {
             int letterCode = str.charAt(i);
@@ -27,18 +39,18 @@ class HashTable {
         return hash;
     }
 
-    int getBucketIndex(String key) {
+    public int getBucketIndex(String key) {
         int capacity = primes[capacityIndex];
         return Math.abs(this.hashCode(key) % capacity);
     }
 
-    boolean loadFactorIsBad() {
+    private boolean loadFactorIsBad() {
         double loadFactor = 0.75;
         int capacity = primes[capacityIndex];
         return (double) size / capacity >= loadFactor;
     }
 
-    void addOrUpdate(String key, int value) {
+    public void addOrUpdate(String key, int value) {
         int bucketIndex = getBucketIndex(key);
         Bucket bucket = buckets[bucketIndex];
         while (bucket != null) {
@@ -53,5 +65,80 @@ class HashTable {
         buckets[bucketIndex] = newNode;
 
         size++;
+    }
+
+    private void doubleBucketSize() {
+        size = 0;
+        Bucket[] oldBucket = buckets;
+        int doubledCapacity = primes[capacityIndex++];
+        buckets = new Bucket[doubledCapacity];
+        for (Bucket bucket : oldBucket) {
+            while (bucket != null) {
+                addOrUpdate(bucket.key, bucket.value);
+                bucket = bucket.next;
+            }
+        }
+    }
+
+    public void insert(String key, int value) {
+        if (loadFactorIsBad()) doubleBucketSize();
+        addOrUpdate(key, value);
+    }
+
+    public Integer get(String key) {
+        int bucketIndex = getBucketIndex(key);
+        Bucket bucketNode = buckets[bucketIndex];
+
+        while (bucketNode != null) {
+            if (bucketNode.key.equals(key)) {
+                return bucketNode.value;
+            }
+            bucketNode = bucketNode.next;
+        }
+        return null;
+    }
+
+    public void remove(String key) {
+        int bucketIndex = getBucketIndex(key);
+        Bucket bucketNode = buckets[bucketIndex];
+        Bucket prev = null;
+
+        while (bucketNode != null) {
+            if (bucketNode.key.equals(key))
+                break;
+            prev = bucketNode;
+            bucketNode = bucketNode.next;
+        }
+
+        Bucket toDelete = bucketNode;
+        if (toDelete == null) return;
+        if (prev != null)
+            buckets[bucketIndex] = toDelete.next;
+        else
+            prev.next = toDelete.next;
+
+        size--;
+    }
+
+    @Override
+    public String toString() {
+        return "HashTable{" +
+                "primes=" + Arrays.toString(primes) +
+                ", size=" + size +
+                ", capacityIndex=" + capacityIndex +
+                ", buckets=" + Arrays.toString(buckets) +
+                '}';
+    }
+}
+class Main {
+    public static void main(String[] args) {
+        var table = new HashTable();
+        table.addOrUpdate("hello", 10);
+        table.addOrUpdate("world", 10);
+        int value = table.get("hello");
+        table.addOrUpdate("hello", 20);
+        value = table.get("hello");
+        System.out.println(value);
+        System.out.println(table);
     }
 }
