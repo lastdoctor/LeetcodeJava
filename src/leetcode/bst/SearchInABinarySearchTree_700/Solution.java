f07732dabd1fba64d7f73c62adc250b8566bc927
Susa Kubatov <dom.ru0@yandex.ru>
2021-11-30 04:53:18 +0500

    700

---
+++ src/leetcode/bst/SearchInABinarySearchTree_700/Solution.java
@@ -0,0 +1,29 @@
+package leetcode.bst.SearchInABinarySearchTree_700;
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public TreeNode searchBST(TreeNode root, int val) {
+        if (root == null) return null;
+        if (root.val == val) return root;
+        return (val < root.val) ? searchBST(root.left, val) : searchBST(root.right, val);
+    }
+}
