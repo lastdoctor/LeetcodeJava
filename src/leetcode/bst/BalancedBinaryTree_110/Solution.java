6552448154d27d4d5586ea693bd8bd1db9b26741
Susa Kubatov <dom.ru0@yandex.ru>
2021-12-02 03:52:59 +0500

    110

---
+++ src/leetcode/bst/BalancedBinaryTree_110/Solution.java
@@ -0,0 +1,38 @@
+package leetcode.bst.BalancedBinaryTree_110;
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) return true;
+        int l = height(root.left);
+        int r = height(root.right);
+
+        return (Math.abs(l - r) <= 1) &&
+                isBalanced(root.left) &&
+                isBalanced(root.right);
+    }
+
+    private int height(TreeNode node) {
+        if (node == null) return 0;
+        return Math.max(height(node.left), height(node.right)) + 1;
+    }
+}
