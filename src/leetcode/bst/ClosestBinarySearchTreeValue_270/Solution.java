27d78c7f7cdca2a9e6185ec125e8b4c8e7c27628
Susa Kubatov <dom.ru0@yandex.ru>
2021-12-09 09:19:48 +0500

    270

---
+++ src/leetcode/bst/ClosestBinarySearchTreeValue_270/Solution.java
@@ -0,0 +1,40 @@
+package leetcode.bst.ClosestBinarySearchTreeValue_270;
+
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    private int prev;
+
+    public int closestValue(TreeNode root, double target) {
+        prev = root.val;
+        find(root, target);
+        return prev;
+    }
+
+    private void find(TreeNode root, double target) {
+        if (root == null) return;
+        find(root.left, target);
+        if (Math.abs(target - root.val) < Math.abs(target - prev)) {
+            prev = root.val;
+        }
+        find(root.right, target);
+    }
+}
