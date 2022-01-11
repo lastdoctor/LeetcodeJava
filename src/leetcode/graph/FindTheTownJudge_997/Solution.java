200f581b39cd069ee20ec768e004660b77e8f176
Kubatov Susa <dom.ru0@yandex.ru>
2022-01-11 22:12:10 +0500

    997

---
+++ src/leetcode/graph/FindTheTownJudge_997/Solution.java
@@ -0,0 +1,16 @@
+package leetcode.graph.FindTheTownJudge_997;
+
+class Solution {
+    public int findJudge(int n, int[][] trust) {
+        int[] trusted = new int[n + 1];
+        for (int[] person : trust) {
+            trusted[person[0]]--;
+            trusted[person[1]]++;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (trusted[i] == n-1) return i;
+        }
+        return -1;
+    }
+}
--- src/leetcode/stack/BasicCalculatorII_227/Solution.java
+++ src/leetcode/stack/BasicCalculatorII_227/Solution.java
@@ -40,4 +40,3 @@ class Solution {
         return ans;
     }
 }
-
