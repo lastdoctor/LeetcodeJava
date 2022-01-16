4cff1a0202a46211fa95e745a2bbf35745708c5a
Kubatov Susa <dom.ru0@yandex.ru>
2022-01-16 16:28:18 +0500

    1971

--- src/leetcode/graph/FindifPathExistsinGraph_1971/Solution.java
+++ src/leetcode/graph/FindifPathExistsinGraph_1971/Solution.java
@@ -2,6 +2,7 @@ package leetcode.graph.FindifPathExistsinGraph_1971;
 
 import java.util.*;
 
+// bfs
 class Solution {
     boolean found = false;
     public boolean validPath(int n, int[][] edges, int start, int end) {
@@ -30,5 +31,33 @@ class Solution {
             }
             if (visited[nei] == false) dfs(graph, visited, nei, end);
         }
+    }
+}
+
+// dfs
+class Solution1 {
+    public boolean validPath(int n, int[][] edges, int start, int end) {
+        if (start == end) return true;
+        var adj = new ArrayList<List<Integer>>();
+        for (int i = 0; i < n; ++i) adj.add(new ArrayList<>());
+        for (int i = 0; i < edges.length; ++i) {
+            adj.get(edges[i][0]).add(edges[i][1]);
+            adj.get(edges[i][1]).add(edges[i][0]);
+        }
+
+        var q = new LinkedList<Integer>();
+        var visited = new HashSet<>();
+        q.addLast(start);
+        while (!q.isEmpty()) {
+            int curr = q.removeFirst();
+            visited.add(curr);
+            if (curr == end) return true;
+            for (int v : adj.get(curr)) {
+                if (!visited.contains(v)) {
+                    q.addLast(v);
+                }
+            }
+        }
+        return false;
     }
 }
