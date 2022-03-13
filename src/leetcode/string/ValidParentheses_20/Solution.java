44e6061c581fd23568a8b6e7003793da28ad1b1d
Kubatov Susa <dom.ru0@yandex.ru>
2022-03-14 04:59:32 +0500

    20

--- src/leetcode/string/ValidParentheses_20/Solution.java
+++ src/leetcode/string/ValidParentheses_20/Solution.java
@@ -18,3 +18,19 @@ class Solution {
         return stack.isEmpty();
     }
 }
+
+class Solution1 {
+    public boolean isValid(String s) {
+        var stack = new LinkedList<Character>();
+        for (char c : s.toCharArray()) {
+            if (c == '(' || c  == '{' || c == '[') stack.addLast(c);
+            else if (!stack.isEmpty() && c == ')' && stack.peekLast() == '(') stack.removeLast();
+            else if (!stack.isEmpty() && c == '}' && stack.peekLast() == '{') stack.removeLast();
+            else if (!stack.isEmpty() && c == ']' && stack.peekLast() == '[') stack.removeLast();
+            else return false;
+        }
+
+
+        return stack.isEmpty();
+    }
+}
