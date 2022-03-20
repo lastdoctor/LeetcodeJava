514a740191ef853bbed02276c5d3ce79d1be0064
Kubatov Susa <dom.ru0@yandex.ru>
2022-03-20 20:18:52 +0500

    316

--- src/leetcode/stack/_316/Solution.java
+++ src/leetcode/stack/_316/Solution.java
@@ -4,34 +4,39 @@ import java.util.*;
 
 class Solution {
     public String removeDuplicateLetters(String s) {
-        var lastIndex = new int[26];
+        var letters = new int[26];
 
-        int idx = 0;
         for (char c : s.toCharArray()) {
-            lastIndex[c - 'a'] = idx;
-            idx++;
+            letters[c - 'a']++;
         }
 
         var seen = new boolean[26];
-        var stack = new LinkedList<Integer>();
+        var stack = new LinkedList<Character>();
 
-        idx = 0;
         for (char c : s.toCharArray()) {
             int curr = c - 'a';
+            letters[curr]--;
             if (seen[curr]) continue;
-            while (!stack.isEmpty() && stack.peekLast() > curr && idx < lastIndex[stack.peekLast()]) {
-                seen[stack.removeLast()] = false;
+            while (!stack.isEmpty() && stack.peekLast() > c && 0 < letters[stack.peekLast() - 'a']) {
+                seen[stack.removeLast() - 'a'] = false;
             }
-            stack.addLast(curr);
+            stack.addLast(c);
             seen[curr] = true;
-            idx++;
         }
 
         var ans = new StringBuilder();
-        for (int с : stack) {
-            ans.append((char) (с + 'a'));
+        for (int value : stack) {
+            ans.append((char) value);
         }
 
         return ans.toString();
+    }
+}
+
+
+class Main {
+    public static void main(String[] arsg) {
+        var s = new Solution().removeDuplicateLetters("cbbbcaa");
+        System.out.println(s);
     }
 }
