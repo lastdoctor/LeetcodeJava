712cc27d581342941280ae8712ba66ec59f8a3f6
Kubatov Susa <dom.ru0@yandex.ru>
2022-03-21 21:18:15 +0500

    763

---
+++ src/leetcode/two_pointers/_763/Solution.java
@@ -0,0 +1,27 @@
+package leetcode.two_pointers._763;
+
+import java.util.*;
+
+class Solution {
+    public List<Integer> partitionLabels(String s) {
+        var map = new HashMap<Character, Integer>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            map.put(c, i);
+        }
+
+        var ans = new ArrayList<Integer>();
+        int prev = -1;
+        int max = 0;
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            max = Math.max(max, map.get(c));
+            if (max == i) {
+                ans.add(max-prev);
+                prev = max;
+            }
+        }
+
+        return ans;
+    }
+}
