52865fa7d99ef9d6e685674029428ed1561f3441
Kubatov Susa <dom.ru0@yandex.ru>
2022-03-27 02:12:35 +0500

    704

--- src/leetcode/array/BinarySearch_704/Solution.java
+++ src/leetcode/array/BinarySearch_704/Solution.java
@@ -14,17 +14,19 @@ class Solution {
 
 class Solution1 {
     public int search(int[] nums, int target) {
-        int start = 0, end = nums.length - 1, mid;
+        int start = 0;
+        int end = nums.length - 1;
+        int mid;
         while (start <= end) {
-            mid = (start + (end - start)) / 2;
-            if (nums[mid] == target) {
-                return mid;
-            } else if (nums[mid] < target) {
+            mid = (end + (end - start)) / 2;
+            if (nums[mid] == target) return mid;
+            if (nums[mid] < target) {
                 start = mid + 1;
             } else {
                 end = mid - 1;
             }
         }
+
         return -1;
     }
 }
