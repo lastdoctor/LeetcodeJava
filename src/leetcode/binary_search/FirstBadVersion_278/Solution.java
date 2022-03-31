8236492db185a2e8c31fc776f16fdad1d7b9add8
Kubatov Susa <dom.ru0@yandex.ru>
2022-04-01 04:25:15 +0500

    278

--- src/leetcode/binary_search/FirstBadVersion_278/Solution.java
+++ src/leetcode/binary_search/FirstBadVersion_278/Solution.java
@@ -23,3 +23,23 @@ public class Solution extends VersionControl {
         return false;
     }
 }
+
+/* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+public class Solutio1 extends VersionControl {
+    public int firstBadVersion(int n) {
+        int start = 1;
+        int end = n;
+        while(start <= end) {
+            int mid = start + (end - start) / 2;
+            if (isBadVersion(mid)) end = mid - 1;
+            else start = mid + 1;
+        }
+        return start;
+    }
+
+    private boolean isBadVersion(int mid) {
+        return false;
+    }
+}
--- src/leetcode/binary_search/_287/Solution.java
+++ src/leetcode/binary_search/_287/Solution.java
@@ -3,8 +3,8 @@ package leetcode.binary_search._287;
 class Solution {
     public int findDuplicate(int[] nums) {
         int start = 1;
-        int end = nums.length-1;
-        while(start <= end) {
+        int end = nums.length - 1;
+        while (start <= end) {
             int mid = start + (end - start) / 2;
             int count = 0;
             for (int num : nums) {
@@ -20,5 +20,12 @@ class Solution {
         }
 
         return start;
+    }
+}
+
+class Main {
+    public static void main(String[] args) {
+        var s = new Solution().findDuplicate(new int[]{1, 3, 4, 2, 2});
+        System.out.println("s = " + s);
     }
 }
