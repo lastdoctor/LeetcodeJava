65e19c3b51985d1adfa7f1b725ace16b4531b73a
Kubatov Susa <dom.ru0@yandex.ru>
2022-04-03 21:02:49 +0500

    557

--- src/leetcode/string/ReverseWordsInAStringIII_557/Solution.java
+++ src/leetcode/string/ReverseWordsInAStringIII_557/Solution.java
@@ -39,3 +39,59 @@ class Solution1 {
         return result.toString();
     }
 }
+
+
+class Solution3 {
+    public String reverseWords(String s) {
+        int n = s.length()-1;
+        var reversedWords = new StringBuilder();
+        var currentWord = new StringBuilder();
+
+        for (char c : s.toCharArray()) {
+            if (c == ' ') {
+                currentWord.reverse();
+                currentWord.append(' ');
+                reversedWords.append(currentWord.toString());
+                currentWord.delete(0, reversedWords.length());
+            } else {
+                currentWord.append(c);
+            }
+        }
+
+        if (currentWord.length() != 0) {
+            currentWord.reverse();
+            reversedWords.append(currentWord.toString());
+        }
+
+        return reversedWords.toString();
+    }
+}
+
+class Solution4 {
+    private String swap(String word, int start, int end) {
+        char[] chars = word.toCharArray();
+
+        char letter = chars[start];
+        chars[start] = chars[end];
+        chars[end] = letter;
+        return new String(chars);
+    }
+
+    public String reverseWords(String s) {
+        var words = s.split(" ");
+        int i;
+        for (i = 0; i < words.length; i++) {
+            String word = words[i];
+            int start = 0;
+            int end = word.length()-1;
+            while(start <= end) {
+                word = swap(word, start, end);
+                start++;
+                end--;
+            }
+            words[i] = word;
+        }
+
+        return String.join(" ", words);
+    }
+}
