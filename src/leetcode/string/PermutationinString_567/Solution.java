29acf973e0d9d5a8ff98b768bb3525a19f8e515e
Kubatov Susa <dom.ru0@yandex.ru>
2022-04-05 21:11:27 +0500

    567

--- src/leetcode/string/PermutationinString_567/Solution.java
+++ src/leetcode/string/PermutationinString_567/Solution.java
@@ -6,26 +6,26 @@ class Solution {
     public boolean checkInclusion(String s1, String s2) {
         if (s1.length() > s2.length()) return false;
 
-        int[] arr1 = new int[26];
-        int[] arr2 = new int[26];
-
+        int[] s1chars = new int[26];
+        int[] s2chars = new int[26];
         for (int i = 0; i < s1.length(); i++) {
-            arr1[s1.charAt(i) - 'a']++;
-            arr2[s2.charAt(i) - 'a']++;
+            s1chars[s1.charAt(i) - 'a']++;
+            s2chars[s2.charAt(i) - 'a']++;
         }
 
-        if (Arrays.equals(arr1, arr2)) return true;
+        if (Arrays.equals(s1chars, s2chars)) return true;
+        int s1start = 0;
+        int s1end = s1.length();
+        while (s1end < s2.length()) {
+            s2chars[s2.charAt(s1start) - 'a']--;
+            s2chars[s2.charAt(s1end) - 'a']++;
 
-        int front = 0;
-        int back = s1.length();
-        while (back < s2.length()) {
-            arr2[s2.charAt(front) - 'a']--;
-            arr2[s2.charAt(back) - 'a']++;
-
-            if (Arrays.equals(arr1, arr2)) return true;
-            front++;
-            back++;
+            if (Arrays.equals(s1chars, s2chars)) return true;
+            s1start++;
+            s1end++;
         }
+
+
         return false;
     }
 }
