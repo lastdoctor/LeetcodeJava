63e9914db2c4e17824c3c57d7c04d10d57b5b57e
Kubatov Susa <dom.ru0@yandex.ru>
2022-04-10 20:15:14 +0500

    347

---
+++ src/leetcode/bucket_sort/_347/Solution.java
@@ -0,0 +1,35 @@
+package leetcode.bucket_sort._347;
+
+import java.util.*;
+
+class Solution {
+    public int[] topKFrequent(int[] nums, int k) {
+        var map = new HashMap<Integer, Integer>();
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        List<Integer> bucket[] = new ArrayList[nums.length + 1];
+        for (int key : map.keySet()) {
+            int freq = map.get(key);
+            if (bucket[freq] == null) {
+                bucket[freq] = new ArrayList<>();
+            }
+            bucket[freq].add(key);
+        }
+
+        int ans[] = new int[k];
+        int index = 0;
+        for (int i = bucket.length - 1; i >= 0; i--) {
+            if (bucket[i] != null) {
+                for (int val : bucket[i]) {
+                    ans[index++] = val;
+                    if (index == k) return ans;
+                }
+            }
+        }
+
+        return ans;
+    }
+}
+
