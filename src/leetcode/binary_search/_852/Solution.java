30a2744c98b85e21d59dc34e909bf36030672a6f
Kubatov Susa <dom.ru0@yandex.ru>
2022-06-04 01:57:26 +0500

    852

---
+++ src/leetcode/binary_search/_852/Solution.java
@@ -0,0 +1,21 @@
+package leetcode.binary_search._852;
+
+class Solution {
+    public int peakIndexInMountainArray(int[] arr) {
+        int start = 0;
+        int end = arr.length - 1;
+        int mid;
+
+        while (start < end) {
+            mid = start + (end - start) / 2;
+
+            if (arr[mid] <= arr[mid + 1]) {
+                start = mid + 1;
+            } else {
+                end = mid;
+            }
+        }
+
+        return start;
+    }
+}
--- src/leetcode/prefix/_303/Solution.java
+++ src/leetcode/prefix/_303/Solution.java
@@ -1,5 +1,12 @@
 package leetcode.prefix._303;
 
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(left,right);
+ */
+
 class NumArray {
     int[] nums;
 
@@ -18,8 +25,17 @@ class NumArray {
     }
 }
 
-/**
- * Your NumArray object will be instantiated and called as such:
- * NumArray obj = new NumArray(nums);
- * int param_1 = obj.sumRange(left,right);
- */
+class NumArray1 {
+    int[] sums;
+
+    public NumArray1(int[] nums) {
+        sums = new int[nums.length + 1];
+        for (int i = 0; i < nums.length; i++) {
+            sums[i + 1] = sums[i] + nums[i];
+        }
+    }
+
+    public int sumRange(int left, int right) {
+        return sums[right + 1] - sums[left];
+    }
+}
