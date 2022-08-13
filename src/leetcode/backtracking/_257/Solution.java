3f96c5707b03ad3b07d62d277e6d7dae0f572d1c
Kubatov Susa <melastdoctorwho@gmail.com>
2022-08-13 17:07:53 +0500

    257

---
+++ src/leetcode/backtracking/_257/Solution.java
@@ -0,0 +1,42 @@
+package leetcode.backtracking._257;
+
+import java.util.*;
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+class Solution {
+    void getTreePaths(TreeNode node, String path, List<String> paths) {
+        if (node.left == null && node.right == null) {
+            paths.add(path + node.val);
+            return;
+        }
+        String candidatePath = path + node.val + "->";
+
+        if (node.left != null) getTreePaths(node.left, candidatePath, paths);
+
+        if (node.right != null) getTreePaths(node.right, candidatePath, paths);
+    }
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        var paths = new LinkedList<String>();
+        getTreePaths(root, "", paths);
+        return paths;
+    }
+}
