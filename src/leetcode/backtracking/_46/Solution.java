1aea931b1bcf7042839529e9d19cd494d97aa948
Kubatov Susa <melastdoctorwho@gmail.com>
2022-08-26 07:10:53 +0500

    46

---
+++ src/leetcode/backtracking/_46/Solution.java
@@ -0,0 +1,24 @@
+package leetcode.backtracking._46;
+
+import java.util.*;
+
+class Solution {
+    void getPermutations(int[] nums, int currentIndex, List<Integer> permutation, List<List<Integer>> permutations) {
+        if (permutation.size() == nums.length) {
+            permutations.add(permutation);
+            return;
+        }
+        for (int i = 0; i <= permutation.size(); i++) {
+            var permutationCandidate = new ArrayList<Integer>(permutation);
+            permutationCandidate.add(i, nums[currentIndex]);
+            getPermutations(nums, currentIndex + 1, permutationCandidate, permutations);
+        }
+    }
+
+    public List<List<Integer>> permute(int[] nums) {
+        var permutations = new ArrayList<List<Integer>>();
+        var permutation = new ArrayList<Integer>();
+        getPermutations(nums, 0, permutation, permutations);
+        return permutations;
+    }
+}
